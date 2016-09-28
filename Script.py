0d53290f3e239c95e1d58e9b571cf988541f6040
Alfredo Hinojosa <alfhh4@gmail.com>
2016-09-28 15:42:14 -0500

    Finch game added}

--- Script.py
+++ Script.py
@@ -3,13 +3,35 @@
 
 from naoProxy import NaoProxy
 from ImageHandler import ImageHandler
-from naoqi import ALBroker 
+from naoqi import ALBroker
+from finchController import FinchController
+from time import sleep
 
 
 class Program():
     
     def __init__(self):
         self.imgHandler = ImageHandler()
+            
+    def strings_to_num(self, argument):
+        switcher = {
+            "uno": 1,
+            "dos": 2,
+            "tres": 3,
+            "cuatro": 4,
+            "cinco": 5,
+        }
+        return switcher.get(argument, 1)
+        
+    def finchGame(self, loopWord):
+        fControl = FinchController()
+        fControl.__init__()
+        
+        x = self.strings_to_num(loopWord)
+        
+        fControl.makeLoop(x)
+        fControl.close()
+            
     
     def scriptIf(self):
         
@@ -25,6 +47,10 @@ class Program():
         self.nao.textAnimatedToSpeech.say("Les explicaré, poco a poco y de forma entretenida. ¿Qué les parece si empezamos con un juego? Para esto, necesitarée un voluntario")
         self.nao.unturn()
         
+        word = self.nao.recognizeWord(["uno", "dos", "tres", "cuatro", "cinco"])
+        self.finchGame(word) # Call the finch game
+        sleep(8)
+        
     def run(self, IP):
         broker = ALBroker("myBroker", "0.0.0.0", 0, IP, 9559)
         
@@ -34,6 +60,7 @@ class Program():
             self.nao = NaoModule1
             self.nao.preloadBehavior("rightHand")
             self.nao.preloadBehavior("leftHand")
+
             
             self.scriptIf()
         except:

cf8bc5722baf38af220271498ed149ebec3a021c
Alfredo Hinojosa <alfhh4@gmail.com>
2016-09-28 15:17:46 -0500

    Merge with new version

--- Script.py
+++ Script.py
@@ -5,6 +5,7 @@ from naoProxy import NaoProxy
 from ImageHandler import ImageHandler
 from naoqi import ALBroker 
 
+
 class Program():
     
     def __init__(self):
@@ -12,14 +13,17 @@ class Program():
     
     def scriptIf(self):
         
-        self.nao.say("Si dicen 'negro', levanto mi mano derecha. Si no, levanto mi mano izquierda.")
-        
-        word = self.nao.recognizeWord(["blanco", "negro"])
-        
-        if (word == "negro"):
-            self.nao.runBehavior("rightHand")
-        else:
-            self.nao.runBehavior("leftHand")
+        self.nao.postureProxy.post.goToPosture("Stand", 1.0)
+        self.nao.say("Buenos días! Mi nombre es Nao")   
+        self.nao.say("El día de hoy, seré su profesor de programación. Espero se diviertan y aprendan mucho.")
+        self.nao.textAnimatedToSpeech.say("^start() La programación, es un tema es muy interesante para mí, pues gracias a la programación, yo existo, puedo hablar, me puedo mover y puedo pensar. Aparte de éstas tareas sencillas, también puedo hacer cosas más complicadas, como hacer yoga y bailar. Y todo esto gracias a la magia de la programación. Les enseñaré. Espero no caerme, ja já ja. ")
+        self.nao.runBehavior("tai")
+        self.nao.textAnimatedToSpeech.say("¿Que les pareció?, Interesante? Cosas como éstas y muchas más se pueden lograr a través de la programación. Hoy les enseñaré a programar, en el lenguaje llamado páiton. Los temas que les enseñaré, hoy son: Variables, condicionales, ciclos y listas.")
+        self.nao.textAnimatedToSpeech.say("Les explicaré, poco a poco y de forma entretenida. ¿Qué les parece si empezamos con un juego? Para esto, necesitarée un voluntario")
+        self.nao.turn()
+        self.nao.runBehavior("turn")
+        self.nao.textAnimatedToSpeech.say("Les explicaré, poco a poco y de forma entretenida. ¿Qué les parece si empezamos con un juego? Para esto, necesitarée un voluntario")
+        self.nao.unturn()
         
     def run(self, IP):
         broker = ALBroker("myBroker", "0.0.0.0", 0, IP, 9559)
--- naoProxy.py
+++ naoProxy.py
@@ -1,6 +1,8 @@
 from naoqi import ALProxy, ALModule, ALBroker
 from threading import Semaphore
 from time import sleep
+import math
+import almath as m # python's wrapping of almath
 
 memory = None
 class NaoProxy(ALModule):
@@ -14,6 +16,9 @@ class NaoProxy(ALModule):
 		self.behaviorManager = ALProxy("ALBehaviorManager")
 		self.speechRecognition = ALProxy("ALSpeechRecognition")
 		self.textToSpeech = ALProxy("ALTextToSpeech")
+		self.textAnimatedToSpeech = ALProxy("ALAnimatedSpeech")		
+		self.postureProxy = ALProxy("ALRobotPosture")
+		self.motionProxy = ALProxy("ALMotion")
 		
 		self.touchedSensors = []
 		self.touchSemaphore = Semaphore(0)
@@ -51,9 +56,9 @@ class NaoProxy(ALModule):
 		
 		memory.subscribeToEvent("WordRecognized","NaoModule1","onWordRecognized")
 		self.wordSemaphore.acquire()
- 		
+		
 		return self.recognizedWord
- 	
+	
 	def onWordRecognized(self, name, value, subscriber):
 		"""	
 			Called when word is recognized
@@ -64,6 +69,28 @@ class NaoProxy(ALModule):
 		
 	def say(self, string):
 		self.textToSpeech.say(string)
+		
+	def turn(self):
+		self.motionProxy.wakeUp()
+		self.postureProxy.goToPosture("StandInit", 0.5)
+		self.motionProxy.setMoveArmsEnabled(True, True)
+		self.motionProxy.setMotionConfig([["ENABLE_FOOT_CONTACT_PROTECTION", True]])
+		X = 0.0
+		Y = 0.0
+		Theta = (math.pi/3.0)*(-1.0)
+		self.motionProxy.post.moveTo(X, Y, Theta)
+		self.motionProxy.waitUntilMoveIsFinished()
+		
+	def unturn(self):
+		self.motionProxy.wakeUp()
+		self.postureProxy.goToPosture("StandInit", 0.5)
+		self.motionProxy.setMoveArmsEnabled(True, True)
+		self.motionProxy.setMotionConfig([["ENABLE_FOOT_CONTACT_PROTECTION", True]])
+		X = 0.0
+		Y = 0.0
+		Theta = (math.pi/3.0)
+		self.motionProxy.post.moveTo(X, Y, Theta)
+		self.motionProxy.waitUntilMoveIsFinished()
 		
 	def setVocabulary(self, vocabulary):
 		self.speechRecognition.setVocabulary(vocabulary, False)
